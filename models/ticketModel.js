18163db18b0202a96a55047e5c8c43e429e59ef1
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-02-26 20:57:46 +0000

    ticket routes

---
+++ models/ticketModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    project: {
+      type: String,
+      required: [true, "Please enter project's name"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter a description"],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["new", "open", "closed"],
+      default: "new",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Ticket", ticketSchema);
---
+++ routes/ticketRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controllers/ticketController");
+const router = express.Router();
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
+
+module.exports = router;
